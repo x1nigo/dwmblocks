58e81146a84a5ff6e4ab03cda358ee5697a548bf
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-08-07 01:33:27 +0800

    Update volume.sh

--- modules/volume.sh
+++ modules/volume.sh
@@ -5,22 +5,22 @@ vol=$(pamixer --get-volume)
 [ $(pamixer --get-mute) = true ] && icon="🔇"
 
 if [ $(pamixer --get-mute) = false ]; then
-	if [ $vol -ge 60 ]; then
-		icon="📢"
-	elif [ $vol -ge 40 ]; then
-		icon="🔊"
-	elif [ $vol -ge 20 ]; then
-		icon="🔉"
-	elif [ $vol -ge 0 ]; then
-		icon="🔈"
-	fi
+        if [ $vol -ge 60 ]; then
+                icon="📢"
+        elif [ $vol -ge 40 ]; then
+                icon="🔊"
+        elif [ $vol -ge 20 ]; then
+                icon="🔉"
+        elif [ $vol -ge 0 ]; then
+                icon="🔈"
+        fi
 fi
 
 case $BUTTON in
-	1) pamixer -t ;;
-	3) st -e vim ~/.config/dwmblocks/modules/volume.sh ;;
-	4) st -e pulsemixer ;;
-	5) st -e vim ~/.config/dwmblocks/blocks.h ;;
+        1) pamixer -t && notify-send -t 2000 "🗣 volume is \`$(pamixer --get-volume-human)\`" ;;
+        3) st -e vim ~/.config/dwmblocks/modules/volume.sh ;;
+        4) st -e pulsemixer ;;
+        5) st -e vim ~/.config/dwmblocks/blocks.h ;;
 esac
 
 printf "%s %s%%\n" "$icon" "$vol"
