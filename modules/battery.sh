32330c26e0e24753faaea4f2fd8d0b9629719c0d
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-08-07 01:30:24 +0800

    Update battery.sh

--- modules/battery.sh
+++ modules/battery.sh
@@ -4,15 +4,15 @@ dir="/sys/class/power_supply/BAT0"
 bat=$(cat $dir/capacity) && stat=$(cat $dir/status)
 
 case $stat in
-	Discharging) icon="🔋" && [ $bat -le 10 ] && warn="❗" && notify-send -t 5000 "❗ Low battery . . ." ;;
-	Charging) icon="🔌" ;;
-	Full) icon="⚡" ;;
+        Discharging) icon="🔋" && [ $bat -le 10 ] && notify-send -t 5000 "❗ warning: low battery, please charge soon!" ;;
+        Charging) icon="🔌" ;;
+        Full) icon="⚡" ;;
 esac
 
 case $BUTTON in
-	1) notify-send -t 2000 $warn$icon $stat ;;
-	3) st -e vim ~/.config/dwmblocks/modules/battery.sh ;;
-	5) st -e vim ~/.config/dwmblocks/blocks.h ;;
+        1) notify-send -t 5000 "$icon battery device is \`$stat\`!" ;;
+        3) st -e vim ~/.config/dwmblocks/modules/battery.sh ;;
+        5) st -e vim ~/.config/dwmblocks/blocks.h ;;
 esac
 
 printf "%s%s %s%%\n" "$warn" "$icon" "$bat"
