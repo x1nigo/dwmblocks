55c12d5047436e4589ab9f7e73c91cb1c795316f
x1nigo <109367483+x1nigo@users.noreply.github.com>
2022-08-07 23:28:09 +0800

    Update blocks.h

--- blocks.h
+++ blocks.h
@@ -1,27 +1,27 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/										/*Update Interval*/	/*Update Signal*/
-	{"", "sh ~/.config/dwmblocks/modules/volume.sh",                        10,     10},
+        /*Icon*/ /*Command*/        /*Update Interval*/ /*Update Signal*/
+        {"", "db-volume",                        10,     10},
 
-	{"", "sh ~/.config/dwmblocks/modules/mic.sh",                           10,      1},
+        {"", "db-mic",                           10,      1},
 
-	{"", "sh ~/.config/dwmblocks/modules/temperature.sh",                    5,     16},
+        {"", "db-temperature",                    5,     16},
 
-	{"", "sh ~/.config/dwmblocks/modules/brightness.sh",                    20,     12},
-	
-	{"", "sh ~/.config/dwmblocks/modules/memory.sh",                         5,      7}, 
+        {"", "db-brightness",                    20,     12},
 
-	{"", "sh ~/.config/dwmblocks/modules/cpu.sh",                            5,      8}, 
+        {"", "db-memory",                         5,      7},
 
-	{"", "sh ~/.config/dwmblocks/modules/nettraffic.sh",                     1,     17},
+        {"", "db-cpu",                            5,      8},
 
-	{"", "sh ~/.config/dwmblocks/modules/battery.sh",                       10,      6},
+        {"", "db-nettraffic",                     1,     17},
 
-	{"", "sh ~/.config/dwmblocks/modules/date.sh",                           5,      9},
-	
-	{"", "sh ~/.config/dwmblocks/modules/network.sh",                       10,     22},
+        {"", "db-battery",                       10,      6},
 
-	{"", "sh ~/.config/dwmblocks/modules/lock.sh",                          25,      4},
+        {"", "db-date",                           5,      9},
+
+        {"", "db-network",                       10,     22},
+
+        {"", "db-lock",                          25,      4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
