986ac18565140180b447c0412bbd0fd17ddf9011
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-04-16 20:47:21 +0000

    Add files via upload

--- blocks.h
+++ blocks.h
@@ -2,34 +2,21 @@
 static const Block blocks[] = {
         /*Icon*/ /*Command*/        /*Update Interval*/ /*Update Signal*/
         {"", "db-volume",                        10,     10},
-
         {"", "db-mic",                           10,      1},
-
         {"", "db-torrent",                        5,     11},
-
         {"", "db-mail",                           5,     20},
-
         {"", "db-temperature",                    5,     16},
-
         {"", "db-brightness",                    20,     12},
-
-        {"", "db-memory",                         5,      7},
-
-        {"", "db-cpu",                            5,      8},
-
+/*        {"", "db-memory",                         5,      7}, */
+/*        {"", "db-cpu",                            5,      8}, */
         {"", "db-nettraffic",                     1,     17},
-
         {"", "db-battery",                       10,      6},
-
         {"", "db-date",                           5,      9},
-
         {"", "db-network",                       10,     22},
-
         {"", "db-system",                        80,     28},
-
         {"", "db-readme",                        50,     14},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "   ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
