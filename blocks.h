6a1831fa09cfb543f145a41312f2f7c2441bdd0a
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-05-31 17:01:41 +0000

    Add files via upload

--- blocks.h
+++ blocks.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/ /*Command*/        /*Update Interval*/ /*Update Signal*/
-        {"", "db-volume",                        10,     10},
-        {"", "db-mic",                           10,      1},
         {"", "db-torrent",                        5,     11},
         {"", "db-mail",                           5,     20},
-        {"", "db-temperature",                    5,     16},
         {"", "db-brightness",                    20,     12},
+        {"", "db-temperature",                    5,     16},
 /*        {"", "db-memory",                         5,      7}, */
 /*        {"", "db-cpu",                            5,      8}, */
         {"", "db-nettraffic",                     1,     17},
+        {"", "db-volume",                         0,     10},
+        {"", "db-mic",                           10,      1},
         {"", "db-battery",                       10,      6},
         {"", "db-date",                           5,      9},
         {"", "db-network",                       10,     22},
@@ -18,5 +18,5 @@ static const Block blocks[] = {
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
