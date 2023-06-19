a958f00482119219ba0d43e2cdf7b775f1ef7051
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-06-19 11:46:52 +0000

    Add files via upload

--- Makefile
+++ Makefile
@@ -16,11 +16,11 @@ options:
 	@echo "LDFLAGS = ${LDFLAGS}"
 	@echo "CC      = ${CC}"
 
-dwmblocks: dwmblocks.c blocks.def.h blocks.h
+dwmblocks: dwmblocks.c config.h
 	${CC} -o dwmblocks dwmblocks.c ${CFLAGS} ${LDFLAGS}
 
-blocks.h:
-	cp blocks.def.h $@
+config.h:
+	cp config.def.h $@
 
 clean:
 	rm -f *.o *.gch dwmblocks
--- config.h
+++ config.h
@@ -3,14 +3,14 @@ static const Block blocks[] = {
         /*Icon*/ /*Command*/        /*Update Interval*/ /*Update Signal*/
         {"", "db-brightness",                    20,     12},
         {"", "db-temperature",                    5,     16},
-        {"", "db-torrent",                        5,     11},
+        {"", "db-torrent",                        5,      2},
         {"", "db-mail",                           5,     20},
 /*        {"", "db-memory",                         5,      7}, */
 /*        {"", "db-cpu",                            5,      8}, */
         {"", "db-nettraffic",                     1,     17},
-        {"", "db-volume",                         0,     10},
-        {"", "db-mic",                            0,      1},
-        {"", "db-battery",                        5,      6},
+        {"", "db-volume",                         5,     10},
+        {"", "db-mic",                            5,     11},
+        {"", "db-battery",                       10,      3},
         {"", "db-date",                           5,      9},
         {"", "db-network",                       10,     22},
         {"", "db-system",                         0,     28},
