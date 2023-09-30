0c58db5d8e41dfc84b2aaf6fb5eb48fa51e91fef
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-10-01 00:26:23 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/ /*Command*/        /*Update Interval*/ /*Update Signal*/
-        {"", "db-brightness",                    20,     12},
-        {"", "db-temperature",                    5,     16},
-        {"", "db-torrent",                        5,      2},
-        {"", "db-mail",                           5,     20},
+/*        {"", "db-brightness",                    20,     12}, */
+/*        {"", "db-temperature",                    5,     16}, */
+/*        {"", "db-torrent",                        5,      2}, */
+/*        {"", "db-mail",                           5,     20}, */
 /*        {"", "db-memory",                         5,      7}, */
 /*        {"", "db-cpu",                            5,      8}, */
-        {"", "db-nettraffic",                     1,     17},
-        {"", "db-volume",                         5,     10},
-        {"", "db-mic",                            5,     11},
+/*        {"", "db-nettraffic",                     1,     17}, */
+/*        {"", "db-volume",                         5,     10}, */
+/*        {"", "db-mic",                            5,     11}, */
         {"", "db-battery",                       10,      3},
         {"", "db-date",                           5,      9},
         {"", "db-network",                       10,     22},
-        {"", "db-readme",                         0,     14},
+/*        {"", "db-readme",                         0,     14}, */
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
