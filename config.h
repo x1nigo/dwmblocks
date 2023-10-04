13f6ef6924503fd600efbae56bbb218c16587abd
Chris Iñigo <109367483+x1nigo@users.noreply.github.com>
2023-10-05 05:06:39 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -1,6 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/ /*Command*/        /*Update Interval*/ /*Update Signal*/
+        {"", "db-brightness",                    60,     12},
+        {"", "db-temp",                          10,      7},
         {"", "db-torrent",                        5,      2},
         {"", "db-mail",                           5,     20},
         {"", "db-nettraffic",                     1,     17},
@@ -9,6 +11,7 @@ static const Block blocks[] = {
         {"", "db-battery",                       10,      3},
         {"", "db-date",                           5,      9},
         {"", "db-network",                       10,     22},
+        {"", "db-readme",                         0,     18},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
