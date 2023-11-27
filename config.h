0746a4595b82bb9e2253e8a1d3f504f9434b66f6
Christian Brodeth <christian.brodeth@gmail.com>
2023-11-28 03:00:14 +0800

    changed stuff

--- config.h
+++ config.h
@@ -1,17 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/ /*Command*/        /*Update Interval*/ /*Update Signal*/
-/*        {"", "db-btc",                          120,     21}, */
-        {"", "db-brightness",                    80,     12},
-        {"", "db-torrent",                       10,      2},
-        {"", "db-mail",                          12,     20},
-        {"", "db-bandwidth",                      1,     17},
-        {"", "db-volume",                         5,     10},
-        {"", "db-mic",                            5,     11},
-        {"", "db-battery",                       10,      3},
-        {"", "db-date",                           5,      9},
-        {"", "db-internet",                       8,     16},
-        {"", "db-readme",                         0,     18},
+        {"", "sl-brightness",                    80,     12},
+        {"", "sl-torrent",                       10,      2},
+        {"", "sl-mail",                          12,     20},
+        {"", "sl-bandwidth",                      1,     17},
+        {"", "sl-volume",                         5,     10},
+        {"", "sl-mic",                            5,     11},
+        {"", "sl-battery",                       10,      3},
+        {"", "sl-date",                           5,      9},
+        {"", "sl-internet",                       8,     16},
+        {"", "sl-readme",                         0,     18},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
