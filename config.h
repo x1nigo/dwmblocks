bc287dd1c62aea8ccbc46fabdec54311371a15fd
x1nigo <109367483+x1nigo@users.noreply.github.com>
2025-05-10 02:29:10 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -1,16 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
         /*Icon*/ /*Command*/        /*Update Interval*/ /*Update Signal*/
-        {"", "sl-brightness",                    80,     12},
-        {"", "sl-torrent",                       10,      2},
-        {"", "sl-mail",                          12,     20},
-        {"", "sl-bandwidth",                      1,     17},
+        {"", "sl-blight",                        80,     12},
+        {"", "sl-traffic",                        1,     17},
         {"", "sl-volume",                         5,     10},
-        {"", "sl-mic",                            5,     11},
         {"", "sl-battery",                       10,      3},
-        {"", "sl-date",                           5,      9},
         {"", "sl-internet",                       8,     16},
-        {"", "sl-readme",                         0,     18},
+        {"", "sl-date",                           5,      9},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
