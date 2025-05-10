54dd18b9fc2e2ee5579844687fa7ee9ebb919b52
x1nigo <109367483+x1nigo@users.noreply.github.com>
2025-05-10 14:01:28 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"",        "sl-blight",	80,		            12},
+	{"",        "sl-traffic",	 1,		             8},
+	{"",        "sl-volume",	 0,		            10},
+	{"",        "sl-battery",	 5,		             7},
+	{"",        "sl-internet",	10,		             5},
+	{"",        "sl-date",  	 5,		            18},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
