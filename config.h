ae71dc58a29e6f8dc0c882bedcf132bdf4488846
x1nigo <109367483+x1nigo@users.noreply.github.com>
2025-05-19 05:58:48 +0800

    Add files via upload

--- config.h
+++ config.h
@@ -2,18 +2,18 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"",        "sl-music",     18,		             4},
-	{"",        "sl-bitcoin",  120,		            21},
+	/* {"",        "sl-bitcoin",  360,		            21}, */
 	{"",        "sl-blight",	80,		            12},
 	{"",        "sl-traffic",	 1,		             8},
 	{"",        "sl-memory",	30,		            16},
 	{"",        "sl-cpu",		15,		            22},
 	{"",        "sl-volume",	 0,		            10},
-	{"",        "sl-battery",	 5,		             7},
+	{"",        "sl-battery",	10,		             7},
 	{"",        "sl-internet",	10,		             5},
 	{"",        "sl-date",  	 5,		            18},
 	{"",        "sl-readme",  	 0,		            28},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = "  ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
