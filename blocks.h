9b01a3f9fab614ddaed6daeb76db09210b90a648
x1nigo <109367483+x1nigo@users.noreply.github.com>
2026-02-24 09:19:24 +0800

    Add files via upload

--- blocks.h
+++ blocks.h
@@ -2,12 +2,16 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "sl-blight",          12,                 12},
+	{"", "sl-mount",           16,                  6},
 	{"", "sl-cpu",             30,                 16},
 	{"", "sl-memory",          30,                 28},
+	{"", "sl-traffic",          1,                 23},
 	{"", "sl-volume",           0,                 10},
+	{"", "sl-microphone",       0,                 10},
 	{"", "sl-disk",           360,                 37},
 	{"", "sl-battery",          5,                  8},
-	{"", "sl-date",             5,                 21},
+	{"", "sl-date",             5,                 27},
+	{"", "sl-internet",         8,                 18},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
